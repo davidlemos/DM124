3994c2d6b5e50c4e77a8e0a9e24c6c9289150939
David Lemos <david.lemos@outlook.com>
2019-09-03 15:15:20 -0300

    Conexão MongoDB

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://davidlemos:dm124@cluster0-lbuc4.mongodb.net/deliveries?retryWrites=true&w=majority', 
+  {
+    useNewUrlParser: true
+  }
+);
+
 app.use(morgan('dev'));
 app.use(express.json());
 
